9bee170a9e2fb21a0b2090e0505496690c2e8ae9
Simos Tigkas <suomikendam@gmail.com>
2025-08-20 12:27:57 +0300

    added Contact and Projects components

---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+    return (
+    <footer id="contact" className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out!!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon"/>
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer">simostigkas@gmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon"/>
+                <a href="https://www.linkedin.com/in/simos-tigkas/">linkedin.com/in/simos-tigkas/</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon"/>
+                <a href="https://www.github.com/SimosTigkas">github.com/SimosTigkas</a>
+            </li>
+        </ul>
+    </footer>);
+};
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Simos!!</h1>
             <p className={styles.description}>I'm a Software Developer who recently completed the studies in Hive. Reach out if you'd like to learn more.</p>
-            <a href="mailto:simostigkas@gmail.com" className={styles.contactBtn}>Contact me!</a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.contactBtn}>Contact me!</a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
         <div className={styles.topBlur}></div>
