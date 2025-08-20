beb45ccb3f6614b263143488e4a5b14c24ba790c
Simos Tigkas <suomikendam@gmail.com>
2025-08-20 12:27:57 +0300

    added Contact and Projects components

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (<div className={styles.App}>
@@ -10,6 +12,8 @@ function App() {
     <Hero />
     <About />
     <Experience />
+    <Projects />
+    <Contact />
   </div>);
 }
 
