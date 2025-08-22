cb0432d7a886502b253f23a3afdfbca1eab67bed
Simos Tigkas <suomikendam@gmail.com>
2025-08-22 10:52:34 +0300

    dixed mobile version issues

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Simos!!</h1>
-            <p className={styles.description}>I'm a Software Developer who recently completed the studies in Hive. Reach out if you'd like to learn more.</p>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.contactBtn}>Contact me!</a>
+            <p className={styles.description}>I'm a Software Developer and I recently completed my studies in Hive. Reach out if you'd like to learn more.</p>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
         <div className={styles.topBlur}></div>
