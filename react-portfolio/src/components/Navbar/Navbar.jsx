45dbb50fee57ebdd0bbf7eff97b6d0fc5d3873e8
Simos Tigkas <suomikendam@gmail.com>
2025-08-22 19:42:34 +0300

    fixed the navbar for desktop

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import styles from '../Navbar/Navbar.module.css';
 import {getImageUrl} from "../../utils";
+import {AiOutlineHome} from "react-icons/ai";
+import {AiOutlineUser} from "react-icons/ai";
+import {BiBook} from "react-icons/bi";
+import { GoProjectSymlink } from "react-icons/go";
+import { LuMessageSquareText } from "react-icons/lu";
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">
-            Home
+            <AiOutlineHome />
         </a>
         <div className={styles.menu}>
             <img className={styles.menuBtn} src={
@@ -18,16 +23,16 @@ export const Navbar = () => {
             />
             <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
                 <li>
-                    <a href="#about">About</a>
+                    <a href="#about"><AiOutlineUser/></a>
+               </li>
+                <li>
+                    <a href="#experience"><BiBook/></a>
                 </li>
                 <li>
-                    <a href="#experience">Experience</a>
+                    <a href="#projects"><GoProjectSymlink/></a>
                 </li>
                 <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
+                    <a href="#contact"><LuMessageSquareText/></a>
                 </li>
             </ul>
         </div>
