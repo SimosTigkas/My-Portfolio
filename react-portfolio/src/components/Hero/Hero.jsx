9ae497dc95f0d210d165382e4603f172a9fb04f7
Simos Tigkas <suomikendam@gmail.com>
2025-08-22 20:54:02 +0300

    added a dowload cv button and changed the function of the contact button

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
+import CV from "../../file/hero/CV.pdf";
 
 export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Simos!!</h1>
             <p className={styles.description}>I'm a Software Developer and I recently completed my studies in Hive. Reach out if you'd like to learn more.</p>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.contactBtn}>Contact me</a>
+            <a href={CV} className={styles.resumeBtn} download>Download CV</a>
+            <a href="#contact" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
         <div className={styles.topBlur}></div>
