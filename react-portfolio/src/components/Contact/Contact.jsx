367942669577cdfb6c9b7160d85ce4726c52f702
Simos Tigkas <suomikendam@gmail.com>
2025-08-29 14:35:11 +0300

    fixed Contact responsiveness

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -6,24 +6,43 @@ import { VscGithub } from "react-icons/vsc";
 
 export const Contact = () => {
     return (
-    <footer id="contact" className={styles.container}>
+        <footer id="contact" className={styles.container}>
         <div className={styles.text}>
             <h2>Contact</h2>
-            <p>Feel free to reach out</p>
+            <p>Feel free to reach out!!</p>
         </div>
         <ul className={styles.links}>
-            <li className={styles.link}>
-                <MdOutlineMail />
-                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com" target="_blank" rel="noopener noreferrer">simostigkas@gmail.com</a>
+            <li>
+                <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=simostigkas@gmail.com"
+                className={styles.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <MdOutlineMail/>
+                </a>
             </li>
-            <li className={styles.link}>
-                <AiOutlineLinkedin />
-                <a href="https://www.linkedin.com/in/simos-tigkas/">linkedin.com/in/simos-tigkas/</a>
+            <li>
+                <a
+                href="https://www.linkedin.com/in/simos-tigkas/"
+                className={styles.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <AiOutlineLinkedin/>
+                </a>
             </li>
-            <li className={styles.link}>
-                <VscGithub />
-                <a href="https://www.github.com/SimosTigkas">github.com/SimosTigkas</a>
+            <li>
+                <a
+                href="https://www.github.com/SimosTigkas"
+                className={styles.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <VscGithub/>
+                </a>
             </li>
         </ul>
-    </footer>);
+    </footer>
+    )
 };
