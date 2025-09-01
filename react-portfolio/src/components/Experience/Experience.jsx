7bdb07114876b5ac34d99c8ca24206168a020865
Simos Tigkas <suomikendam@gmail.com>
2025-09-01 13:50:33 +0300

    added link buttons and fixed exmpty space

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -21,9 +21,8 @@ export const Experience = () => {
           )}
         </div>
         <ul className={styles.history}>
-          {
-            history.map((historyItem, id) => {
-              return <li key={id} className={styles.historyItem}>
+              {history.map((historyItem, id) => {const handleClick = () => {window.open(historyItem.intro, "_blank", "noopener,noreferrer");};
+              return <li key={id} className={styles.historyItem} onClick={handleClick} role="button" tabIndex={0} onKeyDown={(e) => e.key === "Enter" && handleClick()}>
                 <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
